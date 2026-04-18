4ac6cd307f968dcb70a7515d5cbcc919189b558f
H-sai-dheeraj <saidheerajsmartworker@gmail.com>
2026-04-18 21:00:02 +0530

    matrix additon c program

---
+++ matrixsum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int matrix1[3][3],matrix2[3][3],sum[3][3],i,j;
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("Enter the number for [%d][%d]:",i,j);
+            scanf("%d",&matrix1[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("Enter the number for [%d][%d] :",i,j);
+            scanf("%d",&matrix2[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            sum[i][j]=matrix1[i][j]+matrix2[i][j];
+            printf(" %d ",sum[i][j]);
+        }
+        printf("\n");
+    }
+
+}
